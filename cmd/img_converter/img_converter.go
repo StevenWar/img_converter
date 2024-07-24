3c739a197bbed5f68e84a7cb97588fd7525abdc0
StevenWar <stevenwarkentin@web.de>
2024-07-24 20:25:54 +0200

    Add Funktion to convert .png to .ico

--- cmd/img_converter/img_converter.go
+++ cmd/img_converter/img_converter.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"log"
+	"os"
+	"path/filepath"
+
+	ico "github.com/Kodeworks/golang-image-ico"
+)
 
 func main() {
-	fmt.Println("Hello")
+
+	pngFileName := "stick.png"
+	absPath, err := filepath.Abs(pngFileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Open(absPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	img, err := png.Decode(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	rgba := image.NewRGBA(img.Bounds())
+	draw.Draw(rgba, rgba.Bounds(), img, image.Point{}, draw.Src)
+
+	icoFile, err := os.Create("output.ico")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer icoFile.Close()
+
+	err = ico.Encode(icoFile, rgba)
+	if err != nil {
+		log.Fatal(err)
+	}
+	println("Konvertiert")
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/StevenWar/img_converter
 
 go 1.22.5
+
+require github.com/Kodeworks/golang-image-ico v0.0.0-20141118225523-73f0f4cfade9
